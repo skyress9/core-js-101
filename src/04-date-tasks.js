7c1d89f3a33b8f613217f01d08fe6cc40eecc013
skyress9 <lion.super2@gmail.com>
2022-01-06 21:53:32 +0300

    refactor: refactor timeSpanToString function

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -77,8 +77,7 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-  const time = new Date(endDate - startDate);
-  return new Date(time).toISOString().slice(11, 23);
+  return new Date(endDate - startDate).toISOString().slice(11, 23);
 }
 
 
